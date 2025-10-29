47ead9d0985c1fe9c11c80be8d1aa069e7ff1cd4
Ivona <dev.e.vona@gmail.com>
2025-10-29 20:27:12 +0100

    Dovrseni zadatak

---
+++ src/Zadatak.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//    Zadatak 3: Unijeti u program broj ocjena po želji, ispisati prosjek ocjena. (int prosjekOcjena = Integer.parseInt(sc.nextLine());)
+
+public class Zadatak {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Unesite koliko ocjena zelite unijeti:");
+        int kolicinaOcjena = sc.nextInt();
+
+        int[] ocjene = new int[kolicinaOcjena];
+        int zbrojOcjena = 0;
+
+        for (int i = 0; i < ocjene.length; i++) {
+            System.out.println("Unesite ocjenu broj " + (i+1) + ": ");
+            int unesenaOcjena = sc.nextInt();
+            ocjene[i] = unesenaOcjena;
+            zbrojOcjena += unesenaOcjena;
+        }
+
+        int prosjekOcjena = zbrojOcjena / kolicinaOcjena;
+
+        System.out.println("Prosjek unesenih ocjena: " + prosjekOcjena);
+
+    }
+}
